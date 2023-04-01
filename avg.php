bee1990942f8ef465da8e8b8ea79871e5f87dd1c
KryJjjk2 <krystianjanczak02@wp.pl>
2023-04-01 18:09:40 +0200

    srednia wazona

--- avg.php
+++ avg.php
@@ -1,9 +1,13 @@
 <?php
 
-	// pamiętaj o zapisaniu pliku (Crtl+S)
 
 	function srednia(int a, int b)
 	{
 		return (a+b)/2;
+	}
+
+	function wazona_srednia(int a, int b)
+	{
+		return (7*a+3*b)/10;
 	}
 ?>
